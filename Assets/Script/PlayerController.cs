a50df57874f3f082c4e4520f559035969fe69a8b
Barış Mehmet <revisionbilly@gmail.com>
2024-06-26 17:04:52 +0300

    Add Bullet

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,25 +1,50 @@
-using TMPro;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D rb;
-    public TextMeshProUGUI collectedText;
-    public static int collectedAmount;
+    Rigidbody2D rigidbody;
+    public Text collectedText;
+    public static int collectedAmount = 0;
+    public GameObject bulletprefab;
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
 
+
+   // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVert = Input.GetAxis("ShootVertical");
+        if((shootHor != 0 || shootVert != 0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHor, shootVert);
+            lastFire = Time.time;
+        }
         
+        rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
         collectedText.text = "Items Collected: " + collectedAmount;
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletprefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.AddComponent<Rigidbody2D>().velocity = new Vector3 (
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+        0
+    );
     }
 }
